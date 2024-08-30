58fac921c91aa8ae63a685c6618a4e2d3648d400
shuji <shumiyu@example.com>
2024-08-30 15:23:46 +0900

    Describe your changes here

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Stripe\StripeClient;
 use Stripe\Exception\CardException;
 
 class SubscriptionController extends Controller {
